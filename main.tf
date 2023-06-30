47e1af6c62c739d7bbb338876e8d40ff86dcdde0
Sebastian Olsson <sebastian.olsson@algodx.com>
2023-06-30 13:54:50 +0200

    remove crap

--- main.tf
+++ main.tf
@@ -38,3 +38,14 @@ module "aws_eks" {
   ]
   tags = var.tags
 }
+
+module "aws_eks_managed_node_groups" {
+  source = "./modules/aws-eks-managed-node-groups"
+
+  for_each = var.managed_node_groups
+
+  managed_ng = each.value
+  context    = local.node_group_context
+
+  depends_on = [kubernetes_config_map.aws_auth]
+}
