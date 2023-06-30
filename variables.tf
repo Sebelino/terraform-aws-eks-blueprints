65865266df816b1d2008594cfd9dc8eab22db6d2
Sebastian Olsson <sebastian.olsson@algodx.com>
2023-06-30 13:43:14 +0200

    remove crap

--- variables.tf
+++ variables.tf
@@ -51,12 +51,6 @@ variable "managed_node_groups" {
 #-------------------------------
 # aws-auth Config Map
 #-------------------------------
-variable "map_accounts" {
-  description = "Additional AWS account numbers to add to the aws-auth ConfigMap"
-  type        = list(string)
-  default     = []
-}
-
 variable "map_roles" {
   description = "Additional IAM roles to add to the aws-auth ConfigMap"
   type = list(object({
@@ -65,22 +59,6 @@ variable "map_roles" {
     groups   = list(string)
   }))
   default = []
-}
-
-variable "map_users" {
-  description = "Additional IAM users to add to the aws-auth ConfigMap"
-  type = list(object({
-    userarn  = string
-    username = string
-    groups   = list(string)
-  }))
-  default = []
-}
-
-variable "aws_auth_additional_labels" {
-  description = "Additional kubernetes labels applied on aws-auth ConfigMap"
-  type        = map(string)
-  default     = {}
 }
 
 variable "eks_readiness_timeout" {
